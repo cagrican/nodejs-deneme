800956d7519abdd7d5149165b51d140c975a16c7
Çağrı Can Bozkurt <cagricanbozkurt@gmail.com>
2019-09-29 23:00:16 +0300

    eslint upgrade version and swagger update

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,13 @@ import {catchErrors} from "../helpers/errorHandlers";
 const router = express.Router();
 
 /**
- *
  * List records
  * @route Get /api/records
  * @group Record
  * @param {string} startDate.query.required - start date
  * @param {string} endDate.query.required - end Date
- * @param {number} minCount.query.required - end Date
- * @param {number} maxCount.query.required - end Date
+ * @param {number} minCount.query.required - min count
+ * @param {number} maxCount.query.required - max count
  * @returns {object} 200 -
  * @returns {Error}  default - Unexpected error
  */
